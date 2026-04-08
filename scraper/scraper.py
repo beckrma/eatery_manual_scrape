5a08ed7b0452efedbd49b9cee43fd16afbe90696
Ryan Beck <ryanbeck135@gmail.com>
2026-04-08 00:30:16 -0700

    added try/except to prevent invalid links from crashing program

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -47,127 +47,129 @@ with sync_playwright() as p:
         # -------- Iterating Through States --------
         states = page.locator(".state-columns a")
         state_count = states.count()
-    
-        for i in range(state_count):
-            state_href = states.nth(i).get_attribute("href")
-            state_title = states.nth(i).inner_text()
+        try:
+            for i in range(state_count):
+                state_href = states.nth(i).get_attribute("href")
+                state_title = states.nth(i).inner_text()
 
-            city_page = context.new_page()
-            city_page.goto(f"https://www.menufy.com/{state_href}")
+                city_page = context.new_page()
+                city_page.goto(f"https://www.menufy.com/{state_href}")
 
-            cities = city_page.locator(".cities a")
-            cities_count = cities.count()
-            for y in range(cities_count):
-                city_href = cities.nth(y).get_attribute("href")
-                city_title = cities.nth(y).inner_text()
+                cities = city_page.locator(".cities a")
+                cities_count = cities.count()
+                for y in range(cities_count):
+                    city_href = cities.nth(y).get_attribute("href")
+                    city_title = cities.nth(y).inner_text()
 
-                city_rest = context.new_page()
-                city_rest.goto(f"https://www.menufy.com/{city_href}")
+                    city_rest = context.new_page()
+                    city_rest.goto(f"https://www.menufy.com/{city_href}")
 
-                restaurants = city_rest.locator(".list-group a")
-                restaurants_count = restaurants.count()
-                for z in range(restaurants_count):
+                    restaurants = city_rest.locator(".list-group a")
+                    restaurants_count = restaurants.count()
+                    for z in range(restaurants_count):
 
-                    restaurant_cuisine_tags = restaurants.nth(z).locator(".list-group-item-text.cuisines").inner_text()
-                    restaurant_attribute_tags = restaurants.nth(z).locator(".list-group-item-text.attributes").inner_text()
+                        restaurant_cuisine_tags = restaurants.nth(z).locator(".list-group-item-text.cuisines").inner_text()
+                        restaurant_attribute_tags = restaurants.nth(z).locator(".list-group-item-text.attributes").inner_text()
 
-                    rest_href = restaurants.nth(z).get_attribute("href")
+                        rest_href = restaurants.nth(z).get_attribute("href")
 
-                    restaurant = context.new_page()
-                    restaurant.goto(rest_href)
-                    for _ in range(5):
-                        restaurant.evaluate("window.scrollBy(0, window.innerHeight);")
-                        time.sleep(1)
+                        restaurant = context.new_page()
+                        restaurant.goto(rest_href)
+                        for _ in range(5):
+                            restaurant.evaluate("window.scrollBy(0, window.innerHeight);")
+                            time.sleep(1)
 
-            
-                    # -------- Extract Theme, Logo, and Lattitude/Longitude --------
-
-                    logo_meta = restaurant.locator('meta[property="og:image"]')
-                    logo = logo_meta.get_attribute("content")
-
-                    header = restaurant.locator("img.hero-img").get_attribute("src")
-
-                    rest_name = restaurant.title()
-
-                    rest_desc = restaurant.locator('meta[name="description"]')
-                    rest_desc = rest_desc.get_attribute("content")
-
-                    rest_hours = restaurant.locator("#open-hours-root").first.inner_text()
-
-                    extra_hours_info = restaurant.locator(".dropdown-menu.w-full.hours-dropdown").inner_text()
-
-                    rest_phone_num = restaurant.locator('[title="Phone"]').inner_text()
-
-                    rest_address = address = restaurant.locator('a[target="_blank"][href*="maps.google.com"]').inner_text()
-
-                    rest_lat = restaurant.evaluate("window._locationLat")
-                    rest_lng = restaurant.evaluate("window._locationLng")
-
-                    Eatery_DB.insert({
-                        "restaurant": rest_name,
-                        "logo_img": logo,
-                        "header_img": header,
-                        "restaurant_description": rest_desc,
-                        "restaurant_hours": rest_hours,
-                        "extra_hours": extra_hours_info,
-                        "phone_number": rest_phone_num,
-                        "address": rest_address,
-                        "latitude_coordinates": rest_lat,
-                        "longitude_coordinates": rest_lng,
-                        "state": state_title,
-                        "city": city_title,
-                        "cuisine_tags": restaurant_cuisine_tags,
-                        "attribute_tags": restaurant_attribute_tags
-                    }, 2)
-
-                    # -------- Extract all Categories and Menu Items --------
-                    
-                    categories = restaurant.locator("new-menufy-category")
-                    category_count = categories.count()
-
-                    for g in range(category_count):
-                        category = categories.nth(g)
                 
-                        # Grab the category name
-                        cat_name = category.locator("#cat-name").inner_text()
+                        # -------- Extract Theme, Logo, and Lattitude/Longitude --------
 
-                        # Grab the category description (if available)
-                        cat_desc = category.locator("#menu-category-description").inner_text()
-                        
-                        # Grab all items inside this category
-                        items = category.locator("new-menufy-item-card")
-                        item_list = []
-                        for j in range(items.count()):
-                            item = items.nth(j)
-                            item_name = item.locator(".item-name").inner_text()
-                            item_price = item.locator(".item-price span").first.inner_text()
-                            item_description = item.locator(".item-description").inner_text()
-                            style = item.locator(".item-image-wrapper").evaluate("el => el.style.backgroundImage")
-                            image_url = None
-                            if style:
-                                match = re.search(r'url\(["\']?(.*?)["\']?\)', style)
-                                if match:
-                                    image_url = match.group(1)
+                        logo_meta = restaurant.locator('meta[property="og:image"]')
+                        logo = logo_meta.get_attribute("content")
 
-                            item_list.append({
-                                "name": item_name,
-                                "price": item_price,
-                                "description": item_description,
-                                "item_image": image_url
-                            })
-                            items_count += 1
-                    
+                        header = restaurant.locator("img.hero-img").get_attribute("src")
+
+                        rest_name = restaurant.title()
+
+                        rest_desc = restaurant.locator('meta[name="description"]')
+                        rest_desc = rest_desc.get_attribute("content")
+
+                        rest_hours = restaurant.locator("#open-hours-root").first.inner_text()
+
+                        extra_hours_info = restaurant.locator(".dropdown-menu.w-full.hours-dropdown").inner_text()
+
+                        rest_phone_num = restaurant.locator('[title="Phone"]').inner_text()
+
+                        rest_address = address = restaurant.locator('a[target="_blank"][href*="maps.google.com"]').inner_text()
+
+                        rest_lat = restaurant.evaluate("window._locationLat")
+                        rest_lng = restaurant.evaluate("window._locationLng")
+
                         Eatery_DB.insert({
-                            "restaurant": restaurant.title(),
-                            "category": cat_name,
-                            "category_description": cat_desc,
-                            "items": item_list
-                        }, 1)
+                            "restaurant": rest_name,
+                            "logo_img": logo,
+                            "header_img": header,
+                            "restaurant_description": rest_desc,
+                            "restaurant_hours": rest_hours,
+                            "extra_hours": extra_hours_info,
+                            "phone_number": rest_phone_num,
+                            "address": rest_address,
+                            "latitude_coordinates": rest_lat,
+                            "longitude_coordinates": rest_lng,
+                            "state": state_title,
+                            "city": city_title,
+                            "cuisine_tags": restaurant_cuisine_tags,
+                            "attribute_tags": restaurant_attribute_tags
+                        }, 2)
 
-                    restaurant.close()
-                city_rest.close()
-            city_page.close()
-        states.close()
+                        # -------- Extract all Categories and Menu Items --------
+                        
+                        categories = restaurant.locator("new-menufy-category")
+                        category_count = categories.count()
+
+                        for g in range(category_count):
+                            category = categories.nth(g)
+                    
+                            # Grab the category name
+                            cat_name = category.locator("#cat-name").inner_text()
+
+                            # Grab the category description (if available)
+                            cat_desc = category.locator("#menu-category-description").inner_text()
+                            
+                            # Grab all items inside this category
+                            items = category.locator("new-menufy-item-card")
+                            item_list = []
+                            for j in range(items.count()):
+                                item = items.nth(j)
+                                item_name = item.locator(".item-name").inner_text()
+                                item_price = item.locator(".item-price span").first.inner_text()
+                                item_description = item.locator(".item-description").inner_text()
+                                style = item.locator(".item-image-wrapper").evaluate("el => el.style.backgroundImage")
+                                image_url = None
+                                if style:
+                                    match = re.search(r'url\(["\']?(.*?)["\']?\)', style)
+                                    if match:
+                                        image_url = match.group(1)
+
+                                item_list.append({
+                                    "name": item_name,
+                                    "price": item_price,
+                                    "description": item_description,
+                                    "item_image": image_url
+                                })
+                                items_count += 1
+                        
+                            Eatery_DB.insert({
+                                "restaurant": restaurant.title(),
+                                "category": cat_name,
+                                "category_description": cat_desc,
+                                "items": item_list
+                            }, 1)
+
+                        restaurant.close()
+                    city_rest.close()
+                city_page.close()
+            states.close()
+        except Exception as e:
+            print(f"Navigation failed!")
                 
         
     browser.close()
