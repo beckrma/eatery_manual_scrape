bbc9149f3762641f2bdd93d154d9a333f11833eb
Ryan Beck <BECKRM@UCI.EDU>
2026-04-12 16:13:06 -0700

    fixed scraper.py

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -3,11 +3,6 @@ from playwright._impl._errors import TimeoutError
 from urllib.parse import urljoin
 from database.insert_data import EateryDB
 import re
-<<<<<<< HEAD
-import time
-from urllib.parse import urljoin
-=======
->>>>>>> 43555050d4a643f77406e013a21199261fc21c4c
 
 def get_city_links(city_locator):
     cities = []
@@ -46,68 +41,11 @@ def main_scraping(state_list):
                 city_rest_url = urljoin("https://www.menufy.com/", city_link)
                 main_page.goto(city_rest_url, timeout=60000)
 
-<<<<<<< HEAD
-    for r in test_restaurant:
-        print(f"\n🌐 Scraping {r['name']} ...")
-        response = page.goto(r["url"])
-        time.sleep(2)
-
-        # -------- Agentic interactions --------
-        # Try to check consent/accept modals
-        try:
-            page.check("input[type='checkbox']", timeout=2000)
-        except:
-            pass
-        try:
-            page.click("button.accept, button#accept, button.cookie-consent", timeout=2000)
-        except:
-            pass
-
-        # Scroll slowly to load dynamic content
-        for _ in range(5):
-            page.evaluate("window.scrollBy(0, window.innerHeight);")
-            time.sleep(1)
-
-        # -------- Iterating Through States --------
-        states = page.locator(".state-columns a")
-        state_count = states.count()
-    
-        for i in range(state_count):
-            state_href = states.nth(i).get_attribute("href")
-            state_title = states.nth(i).inner_text()
-
-            city_page = context.new_page()
-            city_url = urljoin("https://www.menufy.com/", state_href)
-            city_page.goto(city_url, timeout=60000)
-
-            cities = city_page.locator(".cities a")
-            cities_count = cities.count()
-            for y in range(cities_count):
-                city_href = cities.nth(y).get_attribute("href")
-                city_title = cities.nth(y).inner_text()
-
-                city_rest = context.new_page()
-                city_rest_url = urljoin("https://www.menufy.com/", city_href)
-                city_rest.goto(city_rest_url, timeout=60000)
-
-                restaurants = city_rest.locator(".list-group a")
-                restaurants_count = restaurants.count()
-                for z in range(restaurants_count):
-
-                    restaurant_cuisine_tags = restaurants.nth(z).locator(".list-group-item-text.cuisines").inner_text()
-                    restaurant_attribute_tags = restaurants.nth(z).locator(".list-group-item-text.attributes").inner_text()
-
-                    rest_href = restaurants.nth(z).get_attribute("href")
-                    restaurant = context.new_page()
-                    try:
-                        restaurant.goto(rest_href, timeout=60000)
-=======
                 restaurants = main_page.locator(".list-group a")
                 restaurants_list = get_restaurants(restaurants)
                 for restaurant_cuisine_tags, restaurant_attribute_tags, rest_href in restaurants_list:
                     try:
                         main_page.goto(rest_href, timeout=60000)
->>>>>>> 43555050d4a643f77406e013a21199261fc21c4c
                     except Exception as e:
                         print(f"Navigation failed for {rest_href}: {e}")
                         continue
@@ -115,36 +53,11 @@ def main_scraping(state_list):
                     # -------- Extract Theme, Logo, and Lattitude/Longitude --------
                     try:
                         try:
-<<<<<<< HEAD
-                            logo_meta = restaurant.locator('meta[property="og:image"]')
-=======
                             logo_meta = main_page.locator('meta[property="og:image"]')
->>>>>>> 43555050d4a643f77406e013a21199261fc21c4c
                             logo = logo_meta.get_attribute("content")
                         except TimeoutError as t:
                             logo = "Null"
                         try:
-<<<<<<< HEAD
-                            header = restaurant.locator("img.hero-img").get_attribute("src")
-                        except TimeoutError as t:
-                            header = "Null"
-
-                        rest_name = restaurant.title()
-
-                        rest_desc = restaurant.locator('meta[name="description"]')
-                        rest_desc = rest_desc.get_attribute("content")
-
-                        rest_hours = restaurant.locator("#open-hours-root").first.inner_text()
-
-                        extra_hours_info = restaurant.locator(".dropdown-menu.w-full.hours-dropdown").inner_text()
-
-                        rest_phone_num = restaurant.locator('[title="Phone"]').inner_text()
-
-                        rest_address = address = restaurant.locator('a[target="_blank"][href*="maps.google.com"]').inner_text()
-
-                        rest_lat = restaurant.evaluate("window._locationLat")
-                        rest_lng = restaurant.evaluate("window._locationLng")
-=======
                             header = main_page.locator("img.hero-img").get_attribute("src")
                         except TimeoutError as t:
                             header = "Null"
@@ -171,7 +84,6 @@ def main_scraping(state_list):
                                 return data.address.addressRegion;
                             }
                             """)
->>>>>>> 43555050d4a643f77406e013a21199261fc21c4c
 
                         Eatery_DB.insert({
                             "restaurant": rest_name,
@@ -204,45 +116,6 @@ def main_scraping(state_list):
                     
                             # Grab the category name
                             cat_name = category.locator("#cat-name").inner_text()
-<<<<<<< HEAD
-
-                            # Grab the category description (if available)
-                            cat_desc = category.locator("#menu-category-description").inner_text()
-                            
-                            # Grab all items inside this category
-                            items = category.locator("new-menufy-item-card")
-                            item_list = []
-                            for j in range(items.count()):
-                                item = items.nth(j)
-                                item_name = item.locator(".item-name").inner_text()
-                                item_price = item.locator(".item-price span").first.inner_text()
-                                item_description = item.locator(".item-description").inner_text()
-                                style = item.locator(".item-image-wrapper").evaluate("el => el.style.backgroundImage")
-                                image_url = None
-                                if style:
-                                    match = re.search(r'url\(["\']?(.*?)["\']?\)', style)
-                                    if match:
-                                        image_url = match.group(1)
-
-                                item_list.append({
-                                    "name": item_name,
-                                    "price": item_price,
-                                    "description": item_description,
-                                    "item_image": image_url
-                                })
-                                items_count += 1
-                        
-                            Eatery_DB.insert({
-                                "restaurant": restaurant.title(),
-                                "category": cat_name,
-                                "category_description": cat_desc,
-                                "items": item_list
-                            }, 1)
-                        except Exception as e:
-                            print(f"Menu item scraping failed for {rest_href} : {e}")
-                            continue
-=======
->>>>>>> 43555050d4a643f77406e013a21199261fc21c4c
 
                             # Grab the category description (if available)
                             cat_desc = category.locator("#menu-category-description").inner_text()
